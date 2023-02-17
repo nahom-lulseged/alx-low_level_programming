6d361c8aa9aad3f6eff50878d085725cbcde735e
nahom-lulseged <nahom.lulseged10@gmail.com>
2023-02-17 12:48:57 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		print("%d", a);
+	putchar('\n');
+	return (0);
+}
