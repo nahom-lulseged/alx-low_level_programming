4ebc101e5598ff73b6837ae4305b32970110df10
nahom-lulseged <nahom.lulseged10@gmail.com>
2023-02-17 12:30:33 -0800

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabet.
- * Retrun: Always 0 (Success)
+ * main - Entry point
+ *
+ * Retutn: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
 
+	char d;
+
 	c = 'a';
+	d = 'A';
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(d);
+			d++;
 		}
 	putchar('\n');
 	return (0);
