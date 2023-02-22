c2894ca870ae539a3db24f96781cd8aa13ec3997
nahom-lulseged <nahom.lulseged10@gmail.com>
2023-02-21 16:12:25 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -16,12 +16,12 @@ int main(void)
 		if (i == 0)
 			printf("%ld", j);
 		else if (i == 1)
-			printf("%ld", k);
+			printf(", %ld", k);
 		else
 		{
 			k += j;
 			j = k - j;
-			printf(",%ld", k);
+			printf(", %ld", k);
 		}
 		++i;
 	}
