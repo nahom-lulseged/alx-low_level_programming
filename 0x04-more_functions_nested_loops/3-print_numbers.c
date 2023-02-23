60cd8e103a40b152ca0313bf4997f97693fa8dc4
nahom-lulseged <nahom.lulseged10@gmail.com>
2023-02-23 14:35:47 -0800

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+int c;
+
+for (c = 48; c <= 57; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
