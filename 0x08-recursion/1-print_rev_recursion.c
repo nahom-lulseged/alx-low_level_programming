93a84242e7ac301b5b760232b4ecc4b5c92ae865
nahom-lulseged <nahom.lulseged10@gmail.com>
2023-03-08 11:10:04 -0800

    0x08. C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *  _print_rev_recursion - a function that prints a string in reverse.
- * @s: the string
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s);
+		_putchar(*s);
 	}
 }
