e8fb5ec221547b64f389286574bd3a71fdd16a03
nahom-lulseged <nahom.lulseged10@gmail.com>
2023-03-14 07:40:13 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,42 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * str_concat -  a function that concatenates two strings.
+ * str_concat - function that concatenates two strings.
+ *
  * @s1: first string
  * @s2: second string
  *
- * - The returned pointer should point to a newly allocated space in memory
- *   which contains the contents of s1, followed by the contents of s2, and
- *   null terminated
- * - if NULL is passed, treat it as an empty string
- * - The function should return NULL on failure
- *
- * Return: pointer to the new string array or NULL on failure
+ * Return: NULL on failure
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int i, l1, l2, len, s;
-	char *ar;
+	int len, len1, len2, i, j;
+	char *conStr;
 
 	if (s1 == NULL)
-		l1 = 0;
-	else
-		l1 = strlen(s1);
+		s1 = "";
+	if (s2 == NULL)if (s2 == NULL)
+		s2 = "";
 
-	if (s2 == NULL)
-		l2 = 0;
-	else
-		l2 = strlen(s2);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
-	len = l1 + l2 + 1;
-	ar = malloc(len);
+	len = len1 + len2;
+	conStr = malloc(sizeof(char) * (len + 1));
 
-	if (ar == NULL)
+	if (conStr == NULL)
+	{
+		free(conStr);
 		return (NULL);
-	for (i = 0; i < len; i++)
-	{
-		if (s1 == NULL || s1[i] == '\0')
-			break;
-		ar[i] = s1[i];
 	}
+	for (i = 0; i < len1; i++)
+		conStr[i] = s1[i];
 
-	s = 0;
-	for (; i < len; i++)
-	{
-		if (s2 == NULL)
-			break;
-		ar[i] = s2[s];
-		s++;
-	}
-	return (ar);
-}
+       	j = len2;
+	for (len2 = 0; len2 <= j; i++, len2++)
+		conStr[i] = s2[len2];
+	return (conStr);}
