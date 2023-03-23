9454beaddc41313f3ede6f008e98a54a97b56dcc
nahom-lulseged <nahom.lulseged10@gmail.com>
2023-03-23 07:38:09 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,54 +1,40 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_strings - Prints strings, followed by a new line.
- * @separator: The string to be printed between strings.
- * @n: The number of strings passed to the function.
- * @...: A variable number of strings to be printed.
- *
- * Description: If separator is NULL, it is not printed.
- *              If one of the strings if NULL, (nil) is printed instead.
+ * print_strings - prints strings
+ * @separator: char separator
+ * @n: number of parameters
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list strings;
-	char *str;
-	unsigned int index;
+	va_list args;
+	unsigned int i;
+	char *string;
 
-	va_start(strings, n);
-
-	for (index = 0; index < n; index++)
+	if (n == 0)
 	{
-		str = va_arg(strings, char *);
-
-		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
-
-		if (index != (n - 1) && separator != NULL)
-			printf("%s", separator);
+		printf("\n");
+		return;
 	}
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char *);
+		if (string == NULL)
+			string = "(nil)";
 
-	printf("\n");
-
-	va_end(strings);
-}
- 12  
-0x10-variadic_functions/3-main.c
-Comment on this file
-@@ -0,0 +1,12 @@
-#include "variadic_functions.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_all("ceis", 'B', 3, "stSchool");
-	return (0);
+		if (i != n - 1)
+		{
+			if (separator == NULL)
+				printf("%s", string);
+			else
+				printf("%s%s", string, separator);
+		}
+		else
+			printf("%s\n", string);
+	}
+	va_end(args);
 }
